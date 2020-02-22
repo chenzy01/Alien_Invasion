ad9b52da2b2607a0885122c84e4cae77834e7c80
chenzy01 <chenzy.hyx@gmail.com>
2020-02-22 15:48:37 +0800

    settings

---
+++ settings/settings.py
@@ -0,0 +1,29 @@
+class Settings():
+    """存储 外星人入侵 的所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+
+        # 屏幕设置
+        self.screen_with = 1100  # 屏幕的宽
+        self.screen_hight = 600  # 屏幕的高
+        # 设置背景色
+        """
+        颜色是以RGB值指定，这种颜色由红色、绿色、蓝色值组成，其中每个值的取值范围都在 0~255.通
+        通过组合成不同的RGB值，可创建1600万种颜色
+        红色(255,0,0)
+        绿色(0,255,0)
+        蓝色(0,0,255)
+        """
+        self.bg_color = (230, 230, 230)  # 用浅灰色填充屏幕
+
+        # 飞船的位置
+        self.ship_speed_factor = 1.5
+
+        # 子弹设置:速度，宽，高，颜色
+        self.bullet_speed_factor = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
