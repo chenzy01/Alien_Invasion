6e3796a760f966e2bfdaecd6227c292bac834318
chenzy01 <chenzy.hyx@gmail.com>
2020-03-02 23:58:41 +0800

    增加一些移动的设置

--- settings/settings.py
+++ settings/settings.py
@@ -27,4 +27,11 @@ class Settings():
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
+        # 外星人设置
+        self.alien_speed_fector = 1  # 移动的速度
+        self.fleet_drop_speed = 10  # 向下移动的速度
+        # fleet_direction为1表示向右，-1表示向左
+        self.fleet_direction = 1
+
+
 
