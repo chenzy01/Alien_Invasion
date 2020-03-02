355e874f94bd2850b43f80de82761d43b8d7feaa
chenzy01 <chenzy.hyx@gmail.com>
2020-03-02 23:57:23 +0800

    更新外星人向左或向右移动

--- alien.py
+++ alien.py
@@ -26,3 +26,16 @@ class Alien(Sprite):
         """在指定位置绘制外星人"""
         self.screen.blit(self.image, self.rect)
 
+    def check_edges(self):
+        """如果外星人位于屏幕边缘，就返回True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """向右或向左移动外星人"""
+        self.x += (self.ai_settings.alien_speed_fector * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
+
